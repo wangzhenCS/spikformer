06b4dd056657480d925d4bc3f5e2e913984442a5
wangzhenCS <130069028+wangzhenCS@users.noreply.github.com>
2024-07-12 20:22:11 -0700

    Update model.py
    rollback and change classification number

--- cifar10/model.py
+++ cifar10/model.py
@@ -115,26 +115,26 @@ class SPS(nn.Module):
         self.C = in_channels
         self.H, self.W = self.image_size[0] // patch_size[0], self.image_size[1] // patch_size[1]
         self.num_patches = self.H * self.W
-        self.proj_conv = nn.Conv1d(in_channels, embed_dims//8, kernel_size=3, stride=1, padding=1, bias=False)
-        self.proj_bn = nn.BatchNorm1d(embed_dims//8)
+        self.proj_conv = nn.Conv2d(in_channels, embed_dims//8, kernel_size=3, stride=1, padding=1, bias=False)
+        self.proj_bn = nn.BatchNorm2d(embed_dims//8)
         self.proj_lif = MultiStepLIFNode(tau=2.0, detach_reset=True, backend='torch')
 
-        self.proj_conv1 = nn.Conv1d(embed_dims//8, embed_dims//4, kernel_size=3, stride=1, padding=1, bias=False)
-        self.proj_bn1 = nn.BatchNorm1d(embed_dims//4)
+        self.proj_conv1 = nn.Conv2d(embed_dims//8, embed_dims//4, kernel_size=3, stride=1, padding=1, bias=False)
+        self.proj_bn1 = nn.BatchNorm2d(embed_dims//4)
         self.proj_lif1 = MultiStepLIFNode(tau=2.0, detach_reset=True, backend='torch')
 
-        self.proj_conv2 = nn.Conv1d(embed_dims//4, embed_dims//2, kernel_size=3, stride=1, padding=1, bias=False)
-        self.proj_bn2 = nn.BatchNorm1d(embed_dims//2)
+        self.proj_conv2 = nn.Conv2d(embed_dims//4, embed_dims//2, kernel_size=3, stride=1, padding=1, bias=False)
+        self.proj_bn2 = nn.BatchNorm2d(embed_dims//2)
         self.proj_lif2 = MultiStepLIFNode(tau=2.0, detach_reset=True, backend='torch')
-        self.maxpool2 = torch.nn.MaxPool1d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
+        self.maxpool2 = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
 
-        self.proj_conv3 = nn.Conv1d(embed_dims//2, embed_dims, kernel_size=3, stride=1, padding=1, bias=False)
-        self.proj_bn3 = nn.BatchNorm1d(embed_dims)
+        self.proj_conv3 = nn.Conv2d(embed_dims//2, embed_dims, kernel_size=3, stride=1, padding=1, bias=False)
+        self.proj_bn3 = nn.BatchNorm2d(embed_dims)
         self.proj_lif3 = MultiStepLIFNode(tau=2.0, detach_reset=True, backend='torch')
-        self.maxpool3 = torch.nn.MaxPool1d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
+        self.maxpool3 = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
 
-        self.rpe_conv = nn.Conv1d(embed_dims, embed_dims, kernel_size=3, stride=1, padding=1, bias=False)
-        self.rpe_bn = nn.BatchNorm1d(embed_dims)
+        self.rpe_conv = nn.Conv2d(embed_dims, embed_dims, kernel_size=3, stride=1, padding=1, bias=False)
+        self.rpe_bn = nn.BatchNorm2d(embed_dims)
         self.rpe_lif = MultiStepLIFNode(tau=2.0, detach_reset=True, backend='torch')
 
     def forward(self, x):
@@ -169,7 +169,7 @@ class SPS(nn.Module):
 
 class Spikformer(nn.Module):
     def __init__(self,
-                 img_size_h=32, img_size_w=32, patch_size=16, in_channels=1, num_classes=10,
+                 img_size_h=24, img_size_w=24, patch_size=16, in_channels=1, num_classes=8,
                  embed_dims=64, num_heads=1, mlp_ratios=4, qkv_bias=False, qk_scale=None,
                  drop_rate=0., attn_drop_rate=0., drop_path_rate=0., norm_layer=nn.LayerNorm,
                  depths=6, sr_ratios=2, T = 4
@@ -197,7 +197,7 @@ class Spikformer(nn.Module):
         setattr(self, f"block", block)
 
         # classification head
-        self.head = nn.Linear(embed_dims, 10)
+        self.head = nn.Linear(embed_dims, 8)
         self.apply(self._init_weights)
 
     @torch.jit.ignore
